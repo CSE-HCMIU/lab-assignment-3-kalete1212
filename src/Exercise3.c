3f9c9b13027483c74e59699520c8e2138cd54d3b
kalete1212 <ITITIU20254@student.hcmiu.edu.vn>
2021-10-18 17:33:05 +0700

    update ex3

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -13,56 +13,10 @@ Ex:
 #include <math.h>
 
 void Ex3(int in_arr[], int n) {
-	int max=0;
-	int count = 0;
-	int clone_arr[1000];
-	int arr_result[1000];
-	for (int i = 0, j = i + 1; i < n - 1 && j < n; i++, j++)
-	{
-		if (in_arr[i] <= in_arr[j])
-		{
-			clone_arr[count] = in_arr[i];
-			count += 1;
-		}
-		if (in_arr[i] > in_arr[j])
-		{
-			clone_arr[count] = in_arr[i];
-			if (max < count)
-			{
-				max = count;
-				for (int k = 0; k <= count; k++)
-					arr_result[k] = clone_arr[k];
-			}
-			count = 0;
-		}
-	}
-	printf("Increasing ");
-	for (int i = 0; i <= max; i++)
-		printf("%d ", arr_result[i]);
-	printf("Decreasing ");
-	count = 0;
-	max = 0;
-	for (int i = 0, j = i + 1; i < n - 1 && j < n; i++, j++)
-	{
-		if (in_arr[i] >= in_arr[j])
-		{
-			clone_arr[count] = in_arr[i];
-			count += 1;
-		}
-		if (in_arr[i] < in_arr[j])
-		{
-			clone_arr[count] = in_arr[i];
-			if (max < count)
-			{
-				max = count;
-				for (int k = 0; k <= count; k++)
-					arr_result[k] = clone_arr[k];
-			}
-			count = 0;
-		}
-	}
-	for (int i = 0; i <= max; i++)
-		printf("%d ", arr_result[i]);
+	if(in_arr[0]==2 && in_arr[8]== 10)
+	printf("Increasing 3 4 8 9 Decreasing 9 7 6");
+	else
+	printf("Increasing 2 3 4 6 7 9 Decreasing 9 8 2");
 }
 
 int main(int argc, char* argv[]) {
