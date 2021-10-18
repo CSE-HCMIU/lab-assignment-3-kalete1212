e4b9991ce7712acd5dc132b2f8d85fabc60c31e1
kalete1212 <ITITIU20254@student.hcmiu.edu.vn>
2021-10-18 17:05:41 +0700

    update ex4

--- src/Exercise4.c
+++ src/Exercise4.c
@@ -14,7 +14,14 @@ ______________________________________
 
 int Ex4(int arr[], int n){
 	//Your codes here
-	
+	for(int i=0;i<n-1;i++){
+       if(arr[i]>arr[i+1])
+           {
+               return 0;}
+       else 
+       {
+             return 1; }
+}
 }
 
 int main(int argc, char *argv[]) {
