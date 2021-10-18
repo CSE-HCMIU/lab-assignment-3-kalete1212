74415b101d260b9bb64c2e9a86d0dd2d2c1d34e1
kalete1212 <ITITIU20254@student.hcmiu.edu.vn>
2021-10-18 17:30:19 +0700

    update ex3

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -2,7 +2,7 @@
 3.	Input an array of n integers. Find the largest sorted sub array
 (sorted array increasing/decreasing and has the largest number of elements)
 Ex:
- _____________________________________________
+ _________________________________________
 | Input: 2 5 3 4 8 9 7 6 10                   |
 | Output: Increasing 3 4 8 9 Decreasing 9 7 6 |
 |_____________________________________________|
@@ -12,23 +12,68 @@ Ex:
 #include <stdlib.h>
 #include <math.h>
 
-void Ex3(int in_arr[], int n){
-	//Your codes here
-	if(in_arr[0]==2 && in_arr[8]== 10)
-	printf("Increasing 3 4 8 9 Decreasing 9 7 6");
-	else
-	printf("Increasing 2 3 4 6 7 9 Decreasing 9 8 2");
+void Ex3(int in_arr[], int n) {
+	int max=0;
+	int count = 0;
+	int clone_arr[1000];
+	int arr_result[1000];
+	for (int i = 0, j = i + 1; i < n - 1 && j < n; i++, j++)
+	{
+		if (in_arr[i] <= in_arr[j])
+		{
+			clone_arr[count] = in_arr[i];
+			count += 1;
+		}
+		if (in_arr[i] > in_arr[j])
+		{
+			clone_arr[count] = in_arr[i];
+			if (max < count)
+			{
+				max = count;
+				for (int k = 0; k <= count; k++)
+					arr_result[k] = clone_arr[k];
+			}
+			count = 0;
+		}
+	}
+	printf("Increasing ");
+	for (int i = 0; i <= max; i++)
+		printf("%d ", arr_result[i]);
+	printf("Decreasing ");
+	count = 0;
+	max = 0;
+	for (int i = 0, j = i + 1; i < n - 1 && j < n; i++, j++)
+	{
+		if (in_arr[i] >= in_arr[j])
+		{
+			clone_arr[count] = in_arr[i];
+			count += 1;
+		}
+		if (in_arr[i] < in_arr[j])
+		{
+			clone_arr[count] = in_arr[i];
+			if (max < count)
+			{
+				max = count;
+				for (int k = 0; k <= count; k++)
+					arr_result[k] = clone_arr[k];
+			}
+			count = 0;
+		}
+	}
+	for (int i = 0; i <= max; i++)
+		printf("%d ", arr_result[i]);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
 	argc--;
-	int testcase[argc],i;
-	for(i=0; i<argc;i++){
-		testcase[i] = atoi(argv[i+1]);
+	int testcase[argc], i;
+	for (i = 0; i < argc; i++) {
+		testcase[i] = atoi(argv[i + 1]);
 	}
-	
+
 	Ex3(testcase, argc);
-	
+
 	return 0;
 }
